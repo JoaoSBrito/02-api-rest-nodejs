a7785cd6508a10129a3b8b05c70d7b2c74927b74
joao vitor <joaovbrito123@gmail.com>
2023-06-06 17:47:19 -0300

    add: criação de transações

--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,16 @@
-import { knex as setupKnex, Knex } from 'knex'
+import { Knex, knex as setupKnex } from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: env.DATABASE_URL
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
-    directory: './db/migrations'
-  }
+    directory: './db/migrations',
+  },
 }
 
 export const knex = setupKnex(config)
